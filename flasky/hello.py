e2b92f8ae232b96fa180b19cb385d41178c2a7a0
chy <chyyu7@sina.cn>
2022-05-20 13:41:20 +0800

    新增flask hello

---
+++ flasky/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+# chy
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello world<h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello {}!<h1>'.format(name)
